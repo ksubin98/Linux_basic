6ad44392c60050c8dcbc20db1b02016325a748ac
subinK <subin.k1229@gmail.com>
2021-02-16 12:20:07 +0900

    linux_hw_cmake

---
+++ H3/test/test.cpp
@@ -0,0 +1,6 @@
+#include "test_lib.h"
+using namespace std;
+
+int main(){
+	test_show();
+}
---
+++ H3/test_lib/test_lib.cpp
@@ -0,0 +1,7 @@
+#include "test_lib.h"
+#include <iostream>
+using namespace std;
+
+void test_show(){
+	cout << "학번: 20171530" << endl;
+}
