7a0f4ffa1b615b6603b5893adf87abba1775de11
subinK <76421788+ksubin98@users.noreply.github.com>
2021-02-16 12:29:02 +0900

    Delete H3 directory

--- H3/test/test.cpp
+++
@@ -1,6 +0,0 @@
-#include "test_lib.h"
-using namespace std;
-
-int main(){
-	test_show();
-}
--- H3/test_lib/test_lib.cpp
+++
@@ -1,7 +0,0 @@
-#include "test_lib.h"
-#include <iostream>
-using namespace std;
-
-void test_show(){
-	cout << "학번: 20171530" << endl;
-}
